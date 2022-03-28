3fbf6d7cdc8c9dd0738832af85a69d16a88d0270
Ward M <ward.marissen@gmail.com>
2022-03-28 18:31:14 +0200

    [ADD] sale_order_transport_cost: Add transport costs to order below limit

--- sale_order_transport_cost/__manifest__.py
+++ sale_order_transport_cost/__manifest__.py
@@ -10,14 +10,6 @@
     'data': [
         'data/product_product.xml',
         'views/res_config_settings_views.xml',
-        #
-        # "views/account_move_views.xml",
-        # "views/product_template_views.xml",
-        # "views/sale_order_views.xml",
-        #
-        # "report/account_move_templates.xml",
-        # "report/sale_order_portal_templates.xml",
-        # "report/sale_order_templates.xml",
     ],
     'installable': True,
     'application': True,
