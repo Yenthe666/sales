a822870a6043a99538c8cd3ca916318c72f0240c
Ward M <ward.marissen@gmail.com>
2022-06-08 09:54:06 +0200

    [FIX] update_product_prices_product_category: Log price change with currency

--- update_product_prices_by_category/wizard/product_category_price_update_wizard.py
+++ update_product_prices_by_category/wizard/product_category_price_update_wizard.py
@@ -72,9 +72,9 @@ class ProductCategoryPriceUpdateWizard(models.TransientModel):
             details = self._get_price_details(product)
             body = 'Price update for category %s:<br/><br/>' % self.product_category_id.name
             if details.get('standard_price'):
-                body += 'Sale price: %s -> %s<br/>' % (product.standard_price, details.get('standard_price'))
+                body += 'Sale price: %s -> %s<br/>' % (f'{product.currency_id.symbol} {round(product.lst_price, 2)}', f"{product.currency_id.symbol} {round(details.get('lst_price'), 2)}")
             if details.get('lst_price'):
-                body += 'Cost price: %s -> %s<br/>' % (product.lst_price, details.get('lst_price'))
+                body += 'Cost price: %s -> %s<br/>' % (f'{product.currency_id.symbol} {round(product.standard_price, 2)}', f"{product.currency_id.symbol} {round(details.get('standard_price'), 2)}")
             product.message_post(message_type='comment', body=body)
             product.update(details)
 
