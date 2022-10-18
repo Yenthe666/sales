83b31083f43b9e2e728ce785e618f3a968c21a0a
Swapnesh <swapneshshah111@gmail.com>
2022-10-18 14:28:51 +0530

    [ADD]sale_order_transport_cost: add original module

---
+++ sale_order_transport_cost/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Sale Order Transport Cost',
+    'version': '14.0.0.1',
+    'category': 'Sales',
+    'summary': """Automatically add transport costs to a sale order.""",
+    'description': """Automatically add transport costs to a sale order.""",
+    'author': "Mainframe Monkey",
+    'website': "https://www.mainframemonkey.com",
+    'depends': ['l10n_generic_coa', 'sale_management'],
+    'data': [
+        'data/product_product.xml',
+        'views/res_config_settings_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
