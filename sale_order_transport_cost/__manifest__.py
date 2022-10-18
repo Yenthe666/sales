af805c4c699f533a24c0fb46756c4488ce075373
Swapnesh <swapneshshah111@gmail.com>
2022-10-18 18:44:51 +0530

    [MIG]sale_order_transport_cost: migration to v16

--- sale_order_transport_cost/__manifest__.py
+++ sale_order_transport_cost/__manifest__.py
@@ -1,12 +1,12 @@
 {
     'name': 'Sale Order Transport Cost',
-    'version': '14.0.0.1',
+    'version': '16.0.1.0.0',
     'category': 'Sales',
     'summary': """Automatically add transport costs to a sale order.""",
     'description': """Automatically add transport costs to a sale order.""",
     'author': "Mainframe Monkey",
     'website': "https://www.mainframemonkey.com",
-    'depends': ['l10n_generic_coa', 'sale_management'],
+    'depends': ['sale_management'],
     'data': [
         'data/product_product.xml',
         'views/res_config_settings_views.xml',
--- sale_order_transport_cost/models/sale_order.py
+++ sale_order_transport_cost/models/sale_order.py
@@ -1,63 +1,70 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
+    def _prepare_transport_product(self):
+        return {
+            'name': _('Transport Cost'),
+            'type': 'service',
+            'taxes_id': False,
+            'supplier_taxes_id': False,
+            'sale_ok': True,
+            'purchase_ok': False,
+            'default_code': 'TRANSPORTCOST',
+            'categ_id': self.env.ref('product.product_category_all').id,
+            'list_price': 15,
+            }
+
+    def _prepare_transport_line(self, transport_product):
+        return {
+            'name': _('Transportation costs'),
+            'order_id': self.id,
+            'product_id': transport_product.id,
+            'product_uom': transport_product.uom_id.id,
+            }
+
     @api.model
     def create(self, values):
+        """
+        Add transportation costs to the order if needed
+        """
         sale_orders = super().create(values)
-        for order in sale_orders:
-            order._add_transportation_costs()
+        sale_orders._add_transportation_costs()
         return sale_orders
 
     def _add_transportation_costs(self):
-        if type(self.id) == int:
-            # Get the order total limit for transportation costs
-            order_limit = self.env['ir.config_parameter'].sudo().get_param('sale_order_transport_cost.sale_order_total_for_transport_cost')
-
-            # If there is an order limit configured and the order is a draft, process transport costs
-            if order_limit and order_limit.isdigit() and self.state == 'draft':
+        ICP = self.env['ir.config_parameter'].sudo()
+        so_line_env = self.env['sale.order.line']
+        product_env = self.env['product.product']
+        # Get the order total limit for transportation costs
+        order_limit = ICP.get_param('sale_order_transport_cost.sale_order_total_for_transport_cost')
+        # If there is an order limit configured and the order is a draft, process transport costs
+        if order_limit and order_limit.isdigit():
+            for order in self.filtered(lambda o: o.state == "draft"):
                 # Get the transport product, create it if it doesn't exist
-                transport_product = self.env.ref('sale_order_transport_cost.product_transport_cost', raise_if_not_found=False)
+                transport_product = ICP.get_param('sale_order_transport_cost.product_transport_cost')
                 if not transport_product:
-                    transport_product = self.env['product.product'].sudo().create({
-                        'name': 'Transport Cost',
-                        'type': 'service',
-                        'taxes_id': False,
-                        'supplier_taxes_id': False,
-                        'sale_ok': True,
-                        'purchase_ok': False,
-                        'default_code': 'TRANSPORTCOST',
-                        'categ_id': self.env.ref('product.product_category_all').id,
-                        'list_price': 15,
-                    })
-                    self.env['ir.model.data'].create({
-                        'name': 'product_transport_cost',
-                        'module': 'sale_order_transport_cost',
-                        'res_id': transport_product.id,
-                        'model': 'product.product',
-                        'noupdate': True
-                    })
-
-                # Check if there already is a transport cost
-                transport_line = self.order_line.filtered(lambda line: line.product_id == transport_product)
-
-                # If there is no transport cost and the amount is below the limit, add an order line with the transport cost
-                if self.amount_total < int(order_limit) and not transport_line:
-                    self.env['sale.order.line'].create({
-                        'name': 'Transportation costs',
-                        'order_id': self.id,
-                        'product_id': transport_product.id
-                    })
-
-                # If there is a transport cost, but the total amount is above the limit, remove the transport cost
-                elif transport_line and self.amount_total - transport_line[0].price_subtotal >= int(order_limit) and transport_line:
+                    transport_product = self.env['product.product'].sudo().create(order._prepare_transport_product()).id
+                    ICP.set_param('sale_order_transport_cost.product_transport_cost',
+                                  transport_product)
+                # Check if there already is transport cost
+                transport_product = int(transport_product)
+                transport_line = order.order_line.filtered(lambda line: line.product_id.id == transport_product)
+                # If there is no transport cost and the amount is below the limit, add an order line with the
+                # transport cost
+                if order.amount_total < int(order_limit) and not transport_line:
+                    order_line_vals = order._prepare_transport_line(product_env.browse(transport_product))
+                    so_line_env.create(order_line_vals)
+                # If there is transport cost, but the total amount is above the limit, remove the transport cost
+                elif transport_line and self.amount_total - transport_line[0].price_subtotal >= int(order_limit) and \
+                        transport_line:
                     transport_line.unlink()
 
     def write(self, values):
         """
-        Add transportation costs to the order if needed
+        Add/remove transportation costs to the order if needed
         """
         sale_order = super(SaleOrder, self).write(values)
         self._add_transportation_costs()
