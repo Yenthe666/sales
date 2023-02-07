5280dc41d9812766f124d2e012a56804c85fb3d9
Yenthe666 <yenthevg@gmail.com>
2023-02-07 18:38:19 +0100

    [IMP][1040] quotation_template_customer: Dutch translations

--- quotation_template_customer/models/sale_order_template.py
+++ quotation_template_customer/models/sale_order_template.py
@@ -7,4 +7,7 @@ class SaleOrderTemplate(models.Model):
     customer_ids = fields.Many2many(
         "res.partner",
         string="Customers",
+        help="This template will only be shown on quotations when the customer filled in on the quotation is within "
+             "the list of customers filled in on this quotation template. If this field is left empty the quotation "
+             "template will always be shown/selectable on the quotation."
     )
