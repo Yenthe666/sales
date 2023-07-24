d2be5272296ab6692b118800c1b5d6f91286aabf
Yenthe Van Ginneken <yenthevg@gmail.com>
2023-07-24 14:39:02 +0200

    [IMP] update_product_prices_by_category: set license

--- update_product_prices_by_category/__manifest__.py
+++ update_product_prices_by_category/__manifest__.py
@@ -12,6 +12,7 @@
 
     'author': "Mainframe Monkey",
     'website': "https://www.mainframemonkey.com",
+    'license': 'LGPL-3',
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
